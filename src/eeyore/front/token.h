c5c12d18d2ee869e661035012a96560322272258
MaxXSoft <x@MaxXSoft.net>
2020-10-24 13:25:36 +0800

    (token) added some definitions about token

---
+++ src/eeyore/front/token.h
@@ -0,0 +1,15 @@
+#ifndef MINIVM_EEYORE_FRONT_TOKEN_H_
+#define MINIVM_EEYORE_FRONT_TOKEN_H_
+
+// all supported operators
+#define TOKEN_OPERATORS(e)                                    \
+  e(Add, "+") e(Sub, "-") e(Mul, "*") e(Div, "/") e(Mod, "%") \
+  e(Not, "!") e(Ne, "!=") e(Eq, "==") e(Gt, ">") e(Lt, "<")   \
+  e(Ge, ">=") e(Le, "<=") e(Or, "||") e(And, "&&")
+
+// expand first element to comma-separated list
+#define TOKEN_EXPAND_FIRST(i, ...)      i,
+// expand second element to comma-separated list
+#define TOKEN_EXPAND_SECOND(i, j, ...)  j,
+
+#endif  // MINIVM_EEYORE_FRONT_TOKEN_H_
