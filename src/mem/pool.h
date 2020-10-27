0630be877b3174a0ee2947e604f7fbb70f4b4269
MaxXSoft <x@MaxXSoft.net>
2020-10-27 09:22:34 +0800

    (pool) updated memory pool interface definitions

--- src/mem/pool.h
+++ src/mem/pool.h
@@ -2,6 +2,7 @@
 #define MINIVM_MEM_POOL_H_
 
 #include <memory>
+#include <functional>
 #include <cstdint>
 
 // interface of memory pool
@@ -9,13 +10,18 @@ class MemoryPoolInterface {
  public:
   virtual ~MemoryPoolInterface() = default;
 
-  // allocate a new memory area
+  // allocate a new memory with the specific size
+  // returns the id of allocated memory
   virtual std::uint32_t Allocate(std::uint32_t size) = 0;
-  // get the base address of the specific memory area
-  virtual void *GetAddress(std::uint32_t id) = 0;
+
+  // get the base address of the specific memory
+  // returns 'nullptr' if failed
+  virtual void *GetAddress(std::uint32_t id) const = 0;
 };
 
 // pointer to memory pool
 using MemPoolPtr = std::unique_ptr<MemoryPoolInterface>;
+// factory function of memory pool
+using MemPoolFact = std::function<MemPoolPtr()>;
 
 #endif  // MINIVM_MEM_POOL_H_
