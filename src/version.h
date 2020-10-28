455f098e067697735c88d59dc11a2121b8e99220
MaxXSoft <x@MaxXSoft.net>
2020-10-28 16:16:48 +0800

    (version) added version info

---
+++ src/version.h
@@ -0,0 +1,26 @@
+#ifndef MINIVM_VERSION_H_
+#define MINIVM_VERSION_H_
+
+// version information of MiniVM
+
+#ifndef APP_NAME
+#define APP_NAME            "MiniVM Eeyore/Tigger Virtual Machine"
+#endif
+
+#ifndef APP_VERSION
+#define APP_VERSION         "0.0.0"
+#endif
+
+#ifndef APP_VERSION_MAJOR
+#define APP_VERSION_MAJOR   0
+#endif
+
+#ifndef APP_VERSION_MINOR
+#define APP_VERSION_MINOR   0
+#endif
+
+#ifndef APP_VERSION_PATCH
+#define APP_VERSION_PATCH   0
+#endif
+
+#endif  // MINIVM_VERSION_H_
