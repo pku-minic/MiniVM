09fb8199da0062af29b710b19a9f9a71f0fb65c7
MaxXSoft <x@MaxXSoft.net>
2020-10-29 12:46:25 +0800

    (pool) updated interface of memory pool

--- src/mem/pool.h
+++ src/mem/pool.h
@@ -1,12 +1,8 @@
 #ifndef MINIVM_MEM_POOL_H_
 #define MINIVM_MEM_POOL_H_
 
-#include <optional>
 #include <memory>
-#include <functional>
 #include <cstdint>
-
-#include "vm/define.h"
 
 // type of memory id
 using MemId = std::uint32_t;
@@ -16,24 +12,24 @@ class MemoryPoolInterface {
  public:
   virtual ~MemoryPoolInterface() = default;
 
-  // allocate a new memory with the specific size for a symbol
-  // returns false if failed
-  virtual bool Allocate(SymId sym, std::uint32_t size) = 0;
-  // get id of allocated memory of the specific symbol
-  virtual std::optional<MemId> GetMemId(SymId sym) const = 0;
-  // get the memory base address of the specific symbol
-  // returns 'nullptr' if failed
-  virtual void *GetAddressBySym(SymId sym) const = 0;
+  // allocate a new memory with the specific size
+  // returns memory id
+  virtual MemId Allocate(std::uint32_t size) = 0;
   // get the memory base address of the specific memory id
   // returns 'nullptr' if failed
-  virtual void *GetAddressById(MemId id) const = 0;
+  virtual void *GetAddress(MemId id) const = 0;
+
+  // memory pool state manipulations
+  // handle size of allocated memory only
+  // don't care about the contents of memory
+  //
+  // save current state
+  virtual void SaveState() = 0;
+  // restore the previous state
+  virtual void RestoreState() = 0;
 };
 
 // pointer to memory pool
 using MemPoolPtr = std::unique_ptr<MemoryPoolInterface>;
-// reference to memory pool
-using MemPoolRef = MemoryPoolInterface *;
-// factory function of memory pool
-using MemPoolFact = std::function<MemPoolPtr()>;
 
 #endif  // MINIVM_MEM_POOL_H_
