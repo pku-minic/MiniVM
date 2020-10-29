626f7d5fcff4fd96aaf18ccf880d0f25f9a76e39
MaxXSoft <x@MaxXSoft.net>
2020-10-29 12:51:45 +0800

    (dense) updated the implementations

--- src/mem/dense.h
+++ src/mem/dense.h
@@ -2,35 +2,27 @@
 #define MINIVM_MEM_DENSE_H_
 
 #include <vector>
-#include <unordered_map>
+#include <stack>
 #include <cstdint>
 
 #include "mem/pool.h"
 
 // dense memory pool
 // all memory will be allocated contiguously in one place
-// NOTE:
-// 1. thread unsafe!
-// 2. only allow access to the most recently allocated memory pool
 class DenseMemoryPool : public MemoryPoolInterface {
  public:
-  DenseMemoryPool() : last_mem_size_(mems_.size()) {}
-  ~DenseMemoryPool() { Release(); }
+  DenseMemoryPool() {}
 
-  bool Allocate(SymId sym, std::uint32_t size) override;
-  std::optional<MemId> GetMemId(SymId sym) const override;
-  void *GetAddressBySym(SymId sym) const override;
-  void *GetAddressById(MemId id) const override;
+  MemId Allocate(std::uint32_t size) override;
+  void *GetAddress(MemId id) override;
+  void SaveState() override;
+  void RestoreState() override;
 
  private:
-  void Release();
-
   // all allocated memories
-  static std::vector<std::uint8_t> mems_;
-  // map of symbol id to memory id
-  std::unordered_map<SymId, MemId> ids_;
-  // size of all allocated memories before creating the current pool
-  std::uint32_t last_mem_size_;
+  std::vector<std::uint8_t> mems_;
+  // stack of saved states
+  std::stack<std::uint32_t> states_;
 };
 
 #endif  // MINIVM_MEM_DENSE_H_
