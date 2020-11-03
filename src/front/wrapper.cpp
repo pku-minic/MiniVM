07eccd3b85344402095f57e95f046820237ff4c8
MaxXSoft <x@MaxXSoft.net>
2020-11-03 19:50:10 +0800

    (wrapper) updated some details

--- src/front/wrapper.cpp
+++ src/front/wrapper.cpp
@@ -7,13 +7,13 @@
 
 // defined in Flex/Bison generated files
 extern std::FILE *eeyore_in;
-int eeyore_parse(VMInstContainer &cont);
+int eeyore_parse(void *cont);
 extern std::FILE *tigger_in;
-int tigger_parse(VMInstContainer &cont);
+int tigger_parse(void *cont);
 
 bool ParseEeyore(std::string_view file, VMInstContainer &cont) {
   eeyore_in = fopen(std::string(file).c_str(), "r");
-  auto ret = eeyore_parse(cont);
+  auto ret = eeyore_parse(&cont);
   cont.SealContainer();
   fclose(eeyore_in);
   FreeAllStrs();
@@ -22,7 +22,7 @@ bool ParseEeyore(std::string_view file, VMInstContainer &cont) {
 
 bool ParseTigger(std::string_view file, VMInstContainer &cont) {
   tigger_in = fopen(std::string(file).c_str(), "r");
-  auto ret = tigger_parse(cont);
+  auto ret = tigger_parse(&cont);
   cont.SealContainer();
   fclose(tigger_in);
   FreeAllStrs();
