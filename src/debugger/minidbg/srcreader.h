3372a4873d729c6c5028e2b20cdda283b82c5673
MaxXSoft <x@MaxXSoft.net>
2021-03-26 11:12:30 +0800

    (srcreader) added some details about line number

--- src/debugger/minidbg/srcreader.h
+++ src/debugger/minidbg/srcreader.h
@@ -7,20 +7,27 @@
 #include <unordered_map>
 #include <cstdint>
 
+// source file reader
 class SourceReader {
  public:
-  SourceReader(std::string_view src_file) : ifs_(std::string(src_file)) {}
+  SourceReader(std::string_view src_file) : ifs_(std::string(src_file)) {
+    InitTotalLines();
+  }
 
   // read the content of the specific line number in source file
-  // undefined when 'line_num' is out of range
+  // returns empty string when 'line_num' is out of range
   std::string_view ReadLine(std::uint32_t line_num);
 
  private:
+  // initialize total line number
+  void InitTotalLines();
   // goto the specific line number in file
   void SeekLine(std::uint32_t line_num);
 
   // stream of source file
   std::ifstream ifs_;
+  // total line number
+  std::uint32_t total_lines_;
   // buffer of all read lines
   std::unordered_map<std::uint32_t, std::string> lines_;
 };
