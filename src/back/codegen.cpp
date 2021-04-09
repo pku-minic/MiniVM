e4b4a90940f9032862c5ed5e6f770babb0b182b6
MaxXSoft <x@MaxXSoft.net>
2021-04-09 18:04:29 +0800

    (back/codegen) added class 'CodeGenerator'

---
+++ src/back/codegen.cpp
@@ -0,0 +1,75 @@
+#include "back/codegen.h"
+
+#include <cassert>
+#include <cstddef>
+
+using namespace minivm::back;
+using namespace minivm::vm;
+
+void CodeGenerator::CollectLabelInfo() {
+  // traverse all instructions
+  for (VMAddr i = 0; i < cont_.inst_count(); ++i) {
+    const auto &inst = cont_.insts()[i];
+    // handle by opcode
+    switch (static_cast<InstOp>(inst.op)) {
+      case InstOp::Bnz: {
+        // in-block label
+        labels_.insert(inst.opr);
+        break;
+      }
+      case InstOp::Jmp: {
+        // check if is the first instruction (Jmp kVMEntry)
+        if (!i) {
+          // treat 'kVMEntry' as the main function
+          main_label_ = inst.opr;
+        }
+        else {
+          // in-block label
+          labels_.insert(inst.opr);
+        }
+        break;
+      }
+      case InstOp::Call: {
+        // function label
+        func_labels_.insert(inst.opr);
+        break;
+      }
+      // ignore all other instructions
+      default:;
+    }
+  }
+}
+
+void CodeGenerator::BuildFunctions() {
+  FuncBody *cur_func = nullptr;
+  // traverse all instructions
+  // except the first instruction (Jmp kVMEntry)
+  assert(static_cast<InstOp>(cont_.insts()->op) == InstOp::Jmp);
+  for (VMAddr i = 1; i < cont_.inst_count(); ++i) {
+    // check if is boundary of a function
+    if (i == main_label_) {
+      cur_func = &main_func_;
+    }
+    else if (func_labels_.count(i)) {
+      cur_func = &funcs_.emplace_back();
+    }
+    // push instruction to the current function
+    cur_func->push_back(cont_.insts()[i]);
+  }
+}
+
+void CodeGenerator::Generate() {
+  // reset internal state
+  labels_.clear();
+  func_labels_.clear();
+  funcs_.clear();
+  main_func_.clear();
+  // generate labels & functions
+  CollectLabelInfo();
+  BuildFunctions();
+  // generate on all functions
+  for (const auto &func : funcs_) {
+    GenerateOnFunc(func);
+  }
+  GenerateOnMain(main_func_);
+}
---
+++ src/back/codegen.h
@@ -0,0 +1,61 @@
+#ifndef MINIVM_BACK_CODEGEN_H_
+#define MINIVM_BACK_CODEGEN_H_
+
+#include <ostream>
+#include <vector>
+#include <unordered_set>
+
+#include "vm/instcont.h"
+
+namespace minivm::back {
+
+// code generator for Gopher
+class CodeGenerator {
+ public:
+  CodeGenerator(const vm::VMInstContainer &cont) : cont_(cont) {}
+  virtual ~CodeGenerator() = default;
+
+  // generate code
+  void Generate();
+
+  // dump generated code to the specific stream
+  virtual void Dump(std::ostream &os) const = 0;
+
+ protected:
+  // function body
+  using FuncBody = std::vector<vm::VMInst>;
+
+  // check if there is a label on the specific address
+  bool IsLabel(vm::VMAddr addr) const { return labels_.count(addr); }
+
+  // generate code on function
+  virtual void GenerateOnFunc(const FuncBody &func) = 0;
+  // generate code on main function
+  virtual void GenerateOnMain(const FuncBody &func) = 0;
+
+  // getters
+  const vm::VMInstContainer &cont() const { return cont_; }
+
+ private:
+  // collect all label/function information
+  void CollectLabelInfo();
+  // split code into functions
+  void BuildFunctions();
+
+  // instruction container
+  const vm::VMInstContainer &cont_;
+  // label definitions
+  std::unordered_set<vm::VMAddr> labels_;
+  // function labels
+  std::unordered_set<vm::VMAddr> func_labels_;
+  // label of main function
+  vm::VMAddr main_label_;
+  // functions
+  std::vector<FuncBody> funcs_;
+  // main function
+  FuncBody main_func_;
+};
+
+}  // namespace minivm::back
+
+#endif  // MINIVM_BACK_CODEGEN_H_
