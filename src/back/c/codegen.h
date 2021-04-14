fe967adb38d886ed865108149262bd33b57a53f4
MaxXSoft <x@MaxXSoft.net>
2021-04-14 18:20:55 +0800

    (c/codegen) added implementations of 'CCodeGen'

--- src/back/c/codegen.h
+++ src/back/c/codegen.h
@@ -1,25 +1,42 @@
 #ifndef MINIVM_BACK_C_CODEGEN_H_
 #define MINIVM_BACK_C_CODEGEN_H_
 
+#include <optional>
+#include <string>
 #include <sstream>
 
 #include "back/codegen.h"
+#include "debugger/minidbg/srcreader.h"
 
 namespace minivm::back::c {
 
 // C code generator
 class CCodeGen : public CodeGenerator {
  public:
+  CCodeGen(const vm::VMInstContainer &cont, bool tigger_mode)
+      : CodeGenerator(cont), tigger_mode_(tigger_mode),
+        src_reader_(cont.src_file()) {}
+
   void Dump(std::ostream &os) const override;
 
  protected:
   void Reset() override;
-  void GenerateOnFunc(const FuncBody &func) override;
-  void GenerateOnMain(const FuncBody &func) override;
+  void GenerateOnFunc(vm::VMAddr pc, const FuncBody &func) override;
+  void GenerateOnEntry(vm::VMAddr pc, const FuncBody &func) override;
 
  private:
+  // get symbol name for C code generation
+  std::optional<std::string> GetSymbol(vm::SymId sym_id, vm::VMAddr pc);
+  // generate instruction
+  std::optional<std::string> GenerateInst(vm::VMAddr pc,
+                                          const vm::VMInst &inst);
+
+  // generate Tigger mode code
+  bool tigger_mode_;
   // string stream for storing generated C code
-  std::ostringstream oss_;
+  std::ostringstream global_, code_;
+  // source file reader
+  debugger::minidbg::SourceReader src_reader_;
 };
 
 }  // namespace minivm::back::c
